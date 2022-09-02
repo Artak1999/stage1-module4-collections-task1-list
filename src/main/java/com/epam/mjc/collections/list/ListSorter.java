73538003bdf4fde445541b794ba38df0a800c8c1
Artak1999 <dashyan.1999@mail.ru>
2022-09-02 21:16:47 +0500

    Useful task.

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,22 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        ListComparator listComparator= new ListComparator();
+        for (int i = 0; i < sourceList.size(); i++) {
+            for (int j = 0; j < sourceList.size(); j++) {
+                if(Math.abs(Integer.parseInt(sourceList.get(i))) < Math.abs(Integer.parseInt(sourceList.get(j)))) {
+                    String c = sourceList.get(i);
+                    sourceList.set(i,sourceList.get(j));
+                    sourceList.set(j,c);
+                }
+            }
+        }
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        return a.compareTo(b);
     }
 }
